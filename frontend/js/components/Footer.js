d9c0a5947252d181798167b113a1c2b3a995718a
Rencheng Chua <rench00@gmail.com>
2016-09-12 08:59:01 +0800

    update footer with what jia chuan wanted

--- frontend/js/components/Footer.js
+++ frontend/js/components/Footer.js
@@ -1,10 +1,11 @@
 import React from "react";
 // import {Link} from "react-router"
 
+
 const Footer = () => {
   return(
     <footer>
-          <p class=""></p>
+          <p className="">An initiative by <a href="http://www.conjunctconsulting.org" target="blank">Conjunct Consulting</a> </p>
     </footer>
   )
 }
