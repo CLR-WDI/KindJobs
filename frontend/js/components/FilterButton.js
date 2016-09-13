c0bdd22d9cf415cd0be31834708fa17cee8788da
Christopher Khoo <khooweiwen@gmail.com>
2016-09-13 15:35:20 +0800

    working version of filter buttons

---
+++ frontend/js/components/FilterButton.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const FilterButton = (props) => {
+  return(
+    <button className={this.props._classes} type="button">{this.props._title}</button>
+  )
+}
+
+export default FilterButton;
--- frontend/js/containers/SearchResultsContainer.js
+++ frontend/js/containers/SearchResultsContainer.js
@@ -1,19 +1,33 @@
 import React from "react";
 import SearchBar from "../components/SearchBar";
 import JobList from "../components/JobList";
+import FilterButton from "../components/FilterButton";
 import {Link} from "react-router";
 
 export default class SearchResults extends React.Component {
+  constructor() {
+    super();
+    this._toggleButton = this._toggleButton.bind(this);
+  }
+
+  _toggleButton() {
+    console.log(this.ref)
+  }
+
   render() {
-    console.log(this.props.location.query.search);
     return(
       <div>
         <div class="jumbotron text-center">
-          <h1>Come find your kind of job!</h1>
           <SearchBar _jobListName="Results"/>
           <Link to={'/advsearch'}>Advanced Search</Link>
         </div>
         <div class="container-fluid">
+          <FilterButton _classes="btn btn-fulltime "  ref="fulltime" _title="Full Time" _onClick={this._toggleButton}/>
+          <FilterButton _classes="btn btn-parttime" ref="parttime" _title="Part Time" _onClick={this._toggleButton}/>
+          <FilterButton _classes="btn btn-contract" ref="contract" _title="Contract" _onClick={this._toggleButton}/>
+          <FilterButton _classes="btn btn-project" ref="project" val={true} _title="Project" _onClick={this._toggleButton}/>
+          <FilterButton _classes="btn btn-internship" ref="internship" _title="Internship" _onClick={this._toggleButton}/>
+
           <JobList _jobListName="Results" _searchterms={this.props.location.query.search}/>
         </div>
       </div>
