c8c25591185d647f5330a2a23f283ded64ef0c6c
Rencheng Chua <rench00@gmail.com>
2016-09-14 09:28:24 +0800

    minor cleaning up of code

--- frontend/js/components/Dropdown.js
+++ frontend/js/components/Dropdown.js
@@ -5,7 +5,6 @@ class Dropdown extends React.Component{
 
   render() {
     var sorted_list =this.props._list.sort((a, b) => { return a.name > b.name})
-    console.log(sorted_list);
     let listOptions = sorted_list.map( (listoption) => {
       return <option key={listoption._id} selected={listoption._id === this.props._default} value={listoption._id}>{listoption.name}</option>
     })
--- frontend/js/containers/JobContainer.js
+++ frontend/js/containers/JobContainer.js
@@ -1,7 +1,6 @@
 import React from "react";
 import {connect} from "react-redux"
 import {Link} from "react-router"
-import fakeStore from "../fakeStore"
 import {formatDate} from "../helpers/helpers"
 import {fetchKindJob} from "../actions/kindjobActions"
 
