be98f5a6508baa72616d71f826f30e56f47fb4a3
Luqman Mohamed <lukas.woodman@gmail.com>
2016-09-15 15:32:45 +0800

    refinements on the job card styling particularly with the border thickness, color of icons and title. Adjusted the full time color to a more full bodies and richer blue.

--- frontend/js/components/JobCard.js
+++ frontend/js/components/JobCard.js
@@ -7,10 +7,12 @@ const JobCard = (props) => {
   return(
     <div className = {'jobCard '+props.term+' col-md-4 col-sm-6'}>
       <div class="inner">
-        <h1><i class={"jobicon icon-"+props.image}></i></h1>
-        <h4>{props.sector}</h4>
-        <p>{props.title}</p>
-        <p>{description}</p>
+        <i class={"jobicon icon-"+props.image}></i>
+        <div class="content">
+          <h3>{props.title}</h3>
+          <h4><span class="label">{props.sector}</span></h4>
+          <p>{description}</p>
+        </div>
         <Link to={'/kindjobs/'+props.id}>
           <button type="button" class="btn btn-primary">Details</button>
         </Link>
