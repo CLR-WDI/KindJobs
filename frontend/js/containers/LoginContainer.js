b696e9653d542ea4264e5125b7cd2e3eb5b85456
Luqman Mohamed <lukas.woodman@gmail.com>
2016-10-04 21:39:37 +0800

    minor edits

--- frontend/js/containers/LoginContainer.js
+++ frontend/js/containers/LoginContainer.js
@@ -11,11 +11,13 @@ export default class LoginContainer extends React.Component {
     return(
       <div class="container-fluid">
         <div class="col-md-6 col-md-offset-3">
-          <Tabs bsStyle="pills" defaultActiveKey={1} id="loginTabs" className="content-panel">
+          <Tabs bsStyle="pills" defaultActiveKey={1} id="loginTabs" className="content-panel login-panel">
             <Tab eventKey={1} title="Login">
+              <h1>Login</h1>
               <LoginForm />
             </Tab>
             <Tab eventKey={2} title="Signup">
+              <h1>Signup</h1>
               <SignupForm />
             </Tab>
           </Tabs>
